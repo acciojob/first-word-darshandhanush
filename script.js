913569804b7ec29cd2daa57dcc3625e0b9f7e505
darshandhanush <146746929+darshandhanush@users.noreply.github.com>
2024-09-07 22:30:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function firstWord(s) {
   // your code here
-	 if (s.includes(' ')) {
-        return s.split(' ')[0];
-    }
-    return s;
+	if (s.includes(" ")){
+		return s.split(" ")[0]
+	}
+	return s;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
